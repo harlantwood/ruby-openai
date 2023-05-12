c29eb7692fd21ec67150dedd00b667b67a0619a2
Alex <alexrudall@users.noreply.github.com>
2023-05-12 19:30:15 +0800

    Manually finish rebase

--- lib/openai/client.rb
+++ lib/openai/client.rb
@@ -1,56 +1,64 @@
 module OpenAI
   class Client
-    extend OpenAI::HTTP
+    include OpenAI::HTTP
 
-    def initialize(access_token: nil, organization_id: nil, uri_base: nil, request_timeout: nil)
-      OpenAI.configuration.access_token = access_token if access_token
-      OpenAI.configuration.organization_id = organization_id if organization_id
-      OpenAI.configuration.uri_base = uri_base if uri_base
-      OpenAI.configuration.request_timeout = request_timeout if request_timeout
+    NULL_ORGANIZATION_ID = Object.new.freeze
+
+    attr_reader :access_token, :organization_id, :uri_base, :request_timeout
+
+    def initialize(access_token: nil, organization_id: NULL_ORGANIZATION_ID, uri_base: nil, request_timeout: nil)
+      @access_token = access_token || OpenAI.configuration.access_token
+      @organization_id = if organization_id == NULL_ORGANIZATION_ID
+                           OpenAI.configuration.organization_id
+                         else
+                           organization_id
+                         end
+      @uri_base = uri_base || OpenAI.configuration.uri_base
+      @request_timeout = request_timeout || OpenAI.configuration.request_timeout
     end
 
     def chat(parameters: {})
-      OpenAI::Client.json_post(path: "/chat/completions", parameters: parameters)
+      json_post(path: "/chat/completions", parameters: parameters)
     end
 
     def completions(parameters: {})
-      OpenAI::Client.json_post(path: "/completions", parameters: parameters)
+      json_post(path: "/completions", parameters: parameters)
     end
 
     def edits(parameters: {})
-      OpenAI::Client.json_post(path: "/edits", parameters: parameters)
+      json_post(path: "/edits", parameters: parameters)
     end
 
     def embeddings(parameters: {})
-      OpenAI::Client.json_post(path: "/embeddings", parameters: parameters)
+      json_post(path: "/embeddings", parameters: parameters)
     end
 
     def files
-      @files ||= OpenAI::Files.new
+      @files ||= OpenAI::Files.new(client: self)
     end
 
     def finetunes
-      @finetunes ||= OpenAI::Finetunes.new
+      @finetunes ||= OpenAI::Finetunes.new(client: self)
     end
 
     def images
-      @images ||= OpenAI::Images.new
+      @images ||= OpenAI::Images.new(client: self)
     end
 
     def models
-      @models ||= OpenAI::Models.new
+      @models ||= OpenAI::Models.new(client: self)
     end
 
     def moderations(parameters: {})
-      OpenAI::Client.json_post(path: "/moderations", parameters: parameters)
+      json_post(path: "/moderations", parameters: parameters)
     end
 
     def transcribe(parameters: {})
-      OpenAI::Client.multipart_post(path: "/audio/transcriptions", parameters: parameters)
+      multipart_post(path: "/audio/transcriptions", parameters: parameters)
     end
 
     def translate(parameters: {})
-      OpenAI::Client.multipart_post(path: "/audio/translations", parameters: parameters)
+      multipart_post(path: "/audio/translations", parameters: parameters)
     end
   end
 end
